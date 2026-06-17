7b36c0cfc0a8c2ecd1727261cfae53cf6538e26f
Rogério Caetano dos Santos <rogerio.santos@docente.fieb.edu.br>
2026-06-16 22:17:59 -0300

    Criado ambiente de Dev

---
+++ src/main/java/com/itb/inf2an/pizzariarogerio/PizzariarogerioApplication.java
@@ -0,0 +1,13 @@
+package com.itb.inf2an.pizzariarogerio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariarogerioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariarogerioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/inf2an/pizzariarogerio/PizzariarogerioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.inf2an.pizzariarogerio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariarogerioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
