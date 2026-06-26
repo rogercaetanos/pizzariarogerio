d0c348ffb32764f4b87f122aa927fe17b345bc1e
Rogério Caetano dos Santos <rogerio.santos@docente.fieb.edu.br>
2026-06-25 22:47:17 -0300

    Camada model services dem construção

---
+++ src/main/java/com/itb/inf2an/pizzariarogerio/model/services/ProdutoService.java
@@ -0,0 +1,15 @@
+package com.itb.inf2an.pizzariarogerio.model.services;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import com.itb.inf2an.pizzariarogerio.model.entity.Produto;
+
+public class ProdutoService {
+
+    private static List<Produto> PRODUTOS = new ArrayList<>();
+
+
+    
+
+}
