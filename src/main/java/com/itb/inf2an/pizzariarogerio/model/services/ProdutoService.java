cea4428bfea4ecec8f565bcf93d0cabcfa09ead6
Rogério Caetano dos Santos <rogerio.santos@docente.fieb.edu.br>
2026-06-30 22:41:45 -0300

    Camada model service em memória em construção

--- src/main/java/com/itb/inf2an/pizzariarogerio/model/services/ProdutoService.java
+++ src/main/java/com/itb/inf2an/pizzariarogerio/model/services/ProdutoService.java
@@ -1,13 +1,109 @@
 package com.itb.inf2an.pizzariarogerio.model.services;
 
 import java.util.List;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 
 import com.itb.inf2an.pizzariarogerio.model.entity.Produto;
 
 public class ProdutoService {
 
+    // Lista provisória em memória
+
     private static List<Produto> PRODUTOS = new ArrayList<>();
+
+     static {
+
+        Produto p1 = new Produto();
+        p1.setId(1L);
+        p1.setNome("Pizza Calabresa");
+        p1.setValorVenda(BigDecimal.valueOf(45.90));
+
+        Produto p2 = new Produto();
+        p2.setId(2L);
+        p2.setNome("Pizza Portuguesa");
+        p2.setValorVenda(BigDecimal.valueOf(52.90));
+
+        PRODUTOS.add(p1);
+        PRODUTOS.add(p2);
+    }
+
+    // CREATE
+    public Produto salvar(Produto produto) {
+
+        Long novoId = gerarNovoId();
+        produto.setId(novoId);
+
+        PRODUTOS.add(produto);
+
+        return produto;
+    }
+
+    // READ - listar todos
+    public List<Produto> listarTodos() {
+        return PRODUTOS;
+    }
+
+    // READ - buscar por id
+    public Produto buscarPorId(Long id) {
+
+        for (Produto produto : PRODUTOS) {
+
+            if (produto.getId().equals(id)) {
+                return produto;
+            }
+        }
+
+        return null;
+    }
+
+    // UPDATE
+    public Produto atualizar(Long id, Produto produtoAtualizado) {
+
+        Produto produto = buscarPorId(id);
+
+        if (produto != null) {
+
+            produto.setNome(produtoAtualizado.getNome());
+            produto.setValorVenda(produtoAtualizado.getValorVenda());
+
+            return produto;
+        }
+
+        return null;
+    }
+
+    // DELETE
+    public boolean excluir(Long id) {
+
+        Produto produto = buscarPorId(id);
+
+        if (produto != null) {
+            PRODUTOS.remove(produto);
+            return true;
+        }
+
+        return false;
+    }
+
+    // Gera ID automático
+    private Long gerarNovoId() {
+
+        Long maiorId = 0L;
+
+        for (Produto produto : PRODUTOS) {
+
+            if (produto.getId() > maiorId) {
+                maiorId = produto.getId();
+            }
+        }
+
+        return maiorId + 1;
+    }
+
+    
+
+
 
 
     
